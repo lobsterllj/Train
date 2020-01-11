756035502bf6904204f9d3f95122ec6e5f189893
wurenjie1994 <2687890618@qq.com>
2020-01-11 13:33:04 +0800

    add FaultInjection.cs in git

---
+++ Train/FaultInjection.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Train
+{
+    /// <summary>
+    ///  记录故障注入的一些标志
+    /// </summary>
+    class FaultInjection
+    {
+        private static bool isVersionCompatible = true ;  // M32 version
+        private static bool isNoMessageSent = false ;      // train send no message
+        private static _Q_STATUS qStatus = _Q_STATUS.VALID;
+        private static bool isNoLevelTr = false;  // do not execute level switch
+
+        public static bool IsVersionCompatible
+        {
+            get
+            {
+                return isVersionCompatible;
+            }
+
+            set
+            {
+                isVersionCompatible = value;
+            }
+        }
+
+        public static bool IsNoMessageSent
+        {
+            get
+            {
+                return isNoMessageSent;
+            }
+
+            set
+            {
+                isNoMessageSent = value;
+            }
+        }
+
+        public static _Q_STATUS QStatus
+        {
+            get
+            {
+                return qStatus;
+            }
+
+            set
+            {
+                qStatus = value;
+            }
+        }
+
+        public static bool IsNoLevelTr
+        {
+            get
+            {
+                return isNoLevelTr;
+            }
+
+            set
+            {
+                isNoLevelTr = value;
+            }
+        }
+
+    }
+
+}
